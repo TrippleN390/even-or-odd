24cf206a484c339c2aa5b71da7ca4f021f0377f8
TrippleN <73391335+TrippleN390@users.noreply.github.com>
2022-10-15 14:14:30 +0200

    main.py
    main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# check even or odd
+
+num = 1
+
+while(num < 1000):
+    print(f'elif num == {num}:')
+if (num % 2 == 0):
+    print('\tprint(\'Even\')')
+else:
+    print('\tprint(\'Odd\')')
+    num += 1
